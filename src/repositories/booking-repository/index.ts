14c72e3bf6b93a73b35e9b895deca8b15344ea16
mmparadinha <matheusmparadinha@gmail.com>
2022-11-29 23:17:50 -0300

    Add get /booking route

---
+++ src/repositories/booking-repository/index.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/config";
+
+async function findBookingByUserId(userId: number) {
+  return prisma.booking.findFirst({
+    where: {
+      id: userId,
+    },
+    include: {
+      Room: true,
+    }
+  });
+}
+
+const bookingRepository = {
+  findBookingByUserId,
+};
+
+export default bookingRepository;
---
+++ src/services/bookings-service/index.ts
@@ -0,0 +1,29 @@
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
+import { notFoundError } from "@/errors";
+import bookingRepository from "@/repositories/booking-repository";
+
+async function getBooking(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  
+  if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw notFoundError();
+  }
+
+  const booking = await bookingRepository.findBookingByUserId(userId);
+  return {
+    id: booking.id,
+    Room: booking.Room
+  };
+}
+
+const bookingService = {
+  getBooking,
+};
+
+export default bookingService;
